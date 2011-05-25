1fbc93e5a70b715b11dd3327bdee9f6b4fb7bf86
Daniel Baldwin <dbald@stilito.com>
2011-05-24 22:52:48 -0600

    Removed test code

--- scripts/chromefix.js
+++ scripts/chromefix.js
@@ -1,6 +1,4 @@
 jQuery.noConflict();
 jQuery(function() {
-	jQuery('div#header ul.tabs li span a').removeAttr('onfocus').removeAttr('onclick').click(function() {
-		alert($(this).attr('href'));
-	});
+	jQuery('div#header ul.tabs li span a').removeAttr('onfocus').removeAttr('onclick');
 });
